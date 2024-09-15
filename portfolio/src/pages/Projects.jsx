5180c9c74ea82200fef0c37175adbe12a4641868
greenfireindiagithub <greenfireindia.github@gmail.com>
2024-09-15 21:56:58 +0530

    final last

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -42,7 +42,7 @@ function Projects() {
                             </div>
                         </div>
                     </a>
-                    <a href="https://movie-watchlist-black.vercel.app/" target='_blank'>
+                    <a href="https://one-shop-psi.vercel.app/" target='_blank'>
                     <div className='rounded-xl p-4 bg-[#E9E9E9] dark:bg-[#232323] dark:hover:bg-[#162032]/50 hover:bg-[#fdca40] transition-all duration-300 cursor-pointer flex xl:flex-row flex-col xl:items-center gap-5 group'>
                             <img src="oneshop.png" alt="Movie Watchlist" width='50%' className='rounded-xl xl:w-[25%] md:w-[40%] w-[100%]' />
                             <div className='grid gap-3 w-[100%]'>
@@ -52,7 +52,7 @@ function Projects() {
                                         <p className='text-sm'>Frontend Development</p>
                                     </div>
                                     <div>
-                                        <a href="https://movie-watchlist-black.vercel.app/" target='_blank'>
+                                        <a href="https://one-shop-psi.vercel.app/" target='_blank'>
                                             <span class="material-symbols-outlined p-3 cursor-pointer text-neutral-700 dark:text-[#cfcfcf] dark:hover:text-blue-500 hover:bg-black/10 dark:hover:bg-white/10 rounded-full transition-all duration-300 group-hover:translate-x-2 group-hover:-translate-y-2">
                                                 arrow_outward
                                             </span>
@@ -73,7 +73,7 @@ function Projects() {
                             </div>
                         </div>
                     </a>
-                    <a href="https://one-shop-psi.vercel.app/" target='_blank'>
+                    <a href="#">
                     <div className='rounded-xl p-4 bg-[#E9E9E9] dark:bg-[#232323] dark:hover:bg-[#162032]/50 hover:bg-[#fdca40] transition-all duration-300 cursor-pointer flex xl:flex-row flex-col xl:items-center gap-5 group'>
                             <img src="movie-watchlist.png" alt="Movie Watchlist" width='50%' className='rounded-xl xl:w-[25%] md:w-[40%] w-[100%]' />
                             <div className='grid gap-3 w-[100%]'>
@@ -83,7 +83,7 @@ function Projects() {
                                         <p className='text-sm'>Frontend and Backend Development</p>
                                     </div>
                                     <div>
-                                        <a href="https://one-shop-psi.vercel.app/" target='_blank'>
+                                        <a href="#">
                                             <span class="material-symbols-outlined p-3 cursor-pointer text-neutral-700 dark:text-[#cfcfcf] dark:hover:text-blue-500 hover:bg-black/10 dark:hover:bg-white/10 rounded-full transition-all duration-300 group-hover:translate-x-2 group-hover:-translate-y-2">
                                                 arrow_outward
                                             </span>
@@ -106,57 +106,6 @@ function Projects() {
                             </div>
                         </div>
                     </a>
-                    {/* <a href="https://one-shop-psi.vercel.app/" target='_blank'>
-                        <div className='rounded-xl min-h-[460px] p-4 bg-[#E9E9E9] dark:bg-[#232323] dark:hover:bg-[#162032]/50 flex flex-col gap-5 group cursor-pointer hover:bg-[#fdca40] transition-all duration-300'>
-                            <img src="oneshop.png" alt="Oneshop" width='100%' className='rounded-xl w-[100%]' />
-                            <div className='flex justify-between items-center'>
-                                <div>
-                                    <p className='text-lg'>Oneshop</p>
-                                    <p className='text-sm'>Frontend Development</p>
-                                </div>
-                                <div>
-                                    <a href="https://one-shop-psi.vercel.app/" target='_blank'>
-                                        <span class="material-symbols-outlined p-3 cursor-pointer text-neutral-700 dark:text-[#cfcfcf] dark:hover:text-blue-500 hover:bg-black/10 dark:hover:bg-white/10 rounded-full transition-all duration-300 group-hover:translate-x-2 group-hover:-translate-y-2">
-                                            arrow_outward
-                                        </span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div>
-                                <div className='mt-1 flex gap-2 flex-wrap *:dark:bg-white/10 *:bg-black/10 *:px-3 *:py-1 *:rounded-full *:text-sm'>
-                                    <div><p>Javascript</p></div>
-                                    <div><p>Tailwind CSS</p></div>
-                                    <div><p>HTML</p></div>
-                                    <div><p>CSS</p></div>
-                                </div>
-                                <p className='mt-5 text-sm'>
-                                Developed an e-commerce website with a clean, responsive UI, featuring offers, order management, and personalized user experiences.
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                    <div className='rounded-xl min-h-[460px] p-4 bg-[#E9E9E9] dark:bg-[#232323] dark:hover:bg-[#162032]/50 flex flex-col gap-5 hover:bg-[#fdca40] transition-all duration-300'>
-                        <img src="oneshop.png" alt="NoteIt" width='100%' className='rounded-xl w-[100%]' />
-                        <div className='flex justify-between items-center'>
-                            <div>
-                                <p className='text-lg'>NoteIt</p>
-                                <p className='text-sm'>Frontend and Backend Development</p>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='mt-1 flex gap-2 flex-wrap *:dark:bg-white/10 *:bg-black/10 *:px-3 *:py-1 *:rounded-full *:text-sm'>
-                                <div><p>PHP</p></div>
-                                <div><p>Javascript</p></div>
-                                <div><p>Bootstrap</p></div>
-                                <div><p>MySQL</p></div>
-                                <div><p>HTML</p></div>
-                                <div><p>CSS</p></div>
-                            </div>
-                            <p className='mt-5 text-sm'>
-                            Developed a notes app with features for creating and managing notes, to-dos and event planning, along with additional in-built tools.
-                            </p>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </>
