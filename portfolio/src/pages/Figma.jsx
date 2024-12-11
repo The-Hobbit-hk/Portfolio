24b81cefa4a748f5de9783e5d1433bff0f686ac9
omkarbokil <omkarbokil22@gmail.com>
2024-12-12 00:10:00 +0530

    Figma new design added

--- portfolio/src/pages/Figma.jsx
+++ portfolio/src/pages/Figma.jsx
@@ -9,18 +9,18 @@ function Figma() {
             </h2>
             <div className='grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5'>
                 <div>
-                    <a href="https://www.figma.com/design/G8LfQZyvzMCXX61SOgUWf1/Login-Page?node-id=0-1&t=F7RMRisNHITuxSWP-1" target='_blank' className='block bg-[#E9E9E9] hover:bg-black/20 dark:bg-[#232323] dark:hover:bg-[#fff]/10 p-4 rounded-xl transition-all duration-300 cursor-pointer'>
+                    <a href="https://www.figma.com/proto/0zf9TToldsLohxkAKqI1zr/Game?node-id=1-2&t=mYaF5jFqLo71jQ1s-1" target='_blank' className='block bg-[#E9E9E9] hover:bg-black/20 dark:bg-[#232323] dark:hover:bg-[#fff]/10 p-4 rounded-xl transition-all duration-300 cursor-pointer'>
                         <div className='flex flex-col items-start gap-5'>
-                            <img src="loginpage.png" alt="Movie Watchlist" className='rounded-xl h-44 object-cover w-[100%]' />
-                            <p className='font-semibold'>Login Page</p>
+                            <img src="thegamehub.png" alt="Movie Watchlist" className='rounded-xl h-44 object-cover w-[100%]' />
+                            <p className='font-semibold'>Game Landing Page</p>
                         </div>
                     </a>
                 </div>
                 <div>
-                    <a href="https://www.figma.com/design/0zf9TToldsLohxkAKqI1zr/Game?node-id=0-1&t=xKfNJaYYgW5CjYJh-1" target='_blank' className='block bg-[#E9E9E9] hover:bg-black/20 dark:bg-[#232323] dark:hover:bg-[#fff]/10 p-4 rounded-xl transition-all duration-300 cursor-pointer'>
+                    <a href="https://www.figma.com/proto/Nb02tD9rGb12XiNIi1xKAC/kforkraft?node-id=1-3&t=sA8CRp56xQeCsq7L-1" target='_blank' className='block bg-[#E9E9E9] hover:bg-black/20 dark:bg-[#232323] dark:hover:bg-[#fff]/10 p-4 rounded-xl transition-all duration-300 cursor-pointer'>
                         <div className='flex flex-col items-start gap-5'>
-                            <img src="thegamehub.png" alt="Movie Watchlist" className='rounded-xl h-44 object-cover w-[100%]' />
-                            <p className='font-semibold'>Game Landing Page</p>
+                            <img src="kforkraft.png" alt="Movie Watchlist" className='rounded-xl h-44 object-cover w-[100%]' />
+                            <p className='font-semibold'>Kraft Website</p>
                         </div>
                     </a>
                 </div>
