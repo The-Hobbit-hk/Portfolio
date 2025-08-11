f01d3fe0076f712c58de04c99896a76df373d91d
The-Hobbit-hk <harsh.kale672000@gmail.com>
2025-08-11 20:46:18 +0530

    React

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
             <div className='md:px-40 px-5 py-10 pt-20 md:text-left text-center font-inter dark:text-neutral-300 text-[#343434] flex md:flex-row flex-col items-center gap-5'>
-                <p className='text-sm'>Coded in Cursor. Built with React JS and JAVA. Deployed with Vercel.</p>
+                <p className='text-sm'>Coded in Cursor. Built with JS and JAVA. Deployed with Vercel.</p>
                 <div className='flex gap-2'>
                     <a href="https://www.linkedin.com/in/harshvardhan-kale-9197a01b4/" target='_blank' className='group rounded-full p-3 hover:bg-black/10 dark:hover:bg-white/10'>
                         <FaLinkedinIn className='text-lg text-neutral-700 dark:text-[#cfcfcf] dark:group-hover:text-[#eeeeee] ' />
